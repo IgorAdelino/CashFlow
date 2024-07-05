1aed1783ccd34ecb7107bc1e834c0ce1a849b9c6
IgorAdelino <igoradelino@gmail.com>
2024-07-05 09:11:14 -0300

    feat: unit tests

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,27 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register
+{
+    public class RegisterExpenseValidatorTests
+    {
+        [Fact]
+        public void Success()
+        {
+            var validator = new RegisterExpenseValidator();
+            var request = new RequestRegisterExpenseJson
+            {
+                Amount = 100,
+                Date = DateTime.Now.AddDays(-1),
+                Description = "description",
+                Title = "title",
+                PaymentType = CashFlow.Communication.Enums.PaymentType.Cash
+            };
+
+            var result = validator.Validate(request);
+
+            Assert.True(result.IsValid);
+
+        }
+    }
+}
